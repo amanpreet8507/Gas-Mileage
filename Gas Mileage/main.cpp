5f285997b47420fede28e126ddb09d26b83aedc2
Amanpreet Kaur <amanpreet8507@gmail.com>
2024-11-17 13:20:07 -0800

    first commit

--- Gas Mileage/main.cpp
+++ Gas Mileage/main.cpp
@@ -6,9 +6,46 @@
 //
 
 #include <iostream>
+#include <iomanip>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+int main() {
+    
+    // Intialize variables
+    int milesDriven;
+    int gallonsUsed;
+    int totalMiles{0};
+    int totalGallons{0};
+    
+    // prompt for input and read milesDriven from user
+    cout << "Enter miles Driven(-1 to quit): ";
+    cin >> milesDriven;
+    
+    // Keep taking the input from user while the user inputs -1
+    while(milesDriven != -1){
+        
+        cout << "Enter gallons used: ";
+        cin >> gallonsUsed;
+        
+        // Calculate MPG each trip
+        double mpg{static_cast<double>(milesDriven)/gallonsUsed};
+        
+        // Calculate Total MPG
+        totalMiles += milesDriven;
+        totalGallons += gallonsUsed;
+        double totalMPG{static_cast<double>(totalMiles)/totalGallons};
+        
+        // Output Results
+        cout << setprecision(6) << fixed ;
+        cout << "MPG this trip: " << mpg << endl;
+        
+        cout << setprecision(6) << fixed ;
+        cout << "Total MPG: " << totalMPG << endl << endl;
+        
+        // Prompt the user to enter miles driven and gallons used
+        cout << "Enter miles Driven(-1 to quit): ";
+        cin >> milesDriven;
+    }
+    cout << "Program ended" << endl;
 }
